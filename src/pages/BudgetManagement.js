1655b996161daf97115c48a89dc7da86929e1a28
Victor-DM18 <victor.dosmartires@gmail.com>
2021-04-14 16:33:11 +0200

    docs: Card

---
+++ src/pages/BudgetManagement.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+
+const BudgetManagement = () => {
+    return (
+        <Card>
+            <Card.Body>
+                <Card.Title>Nourriture</Card.Title>
+                <Card.text>TOTAL: €</Card.text>
+            </Card.Body>
+        </Card>
+    );
+};
+
+export default BudgetManagement;
