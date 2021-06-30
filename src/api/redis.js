155d441ac80d0bf40bcd95b9aa4130327ee2eabd
Victor-DM18 <victor.dosmartires@gmail.com>
2021-06-30 19:44:20 +0200

    feat: redis

--- src/api/redis.js
+++ src/api/redis.js
@@ -1,3 +1,6 @@
 import Redis from "ioredis"
-
-export default new Redis()
+export default new Redis({
+  host: process.env.REDIS_HOST,
+  password: process.env.REDIS_PASSWORD,
+  db: process.env.REDIS_DB,
+})
